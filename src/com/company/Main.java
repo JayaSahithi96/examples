a7ded3812ea62b8bbf9dc8fcba8da2026c69e310
sahithi31 <jsahithi90@gmail.com>
2021-01-13 09:44:24 -0800

    class and objects

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        bicycle sportsCycle = new bicycle();
+        bicycle mountainCycle = new bicycle();
+        sportsCycle.braking();
+    }
+}
---
+++ src/com/company/bicycle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class bicycle {
+   private int gear= 5;
+
+    public void braking(){
+        System.out.println("break works");
+    }
+}
+
+
